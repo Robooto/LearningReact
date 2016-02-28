980346752ea8bda1a51b9121a55af692614dbe25
Sam Russell <samrussell63@gmail.com>
2016-02-28 09:22:38 -0600

    initial commit
    Attempting to learn React.  Currently not working.

---
+++ js/components/forum.react.js
@@ -0,0 +1,38 @@
+var Forum = React.createClass({
+    getInitialState: function () {
+        return {
+            allAnswers: ForumStore.getAnswers()
+        }
+    },
+    componentDidMount: function() {
+        //FormStore.addChangeListener(this._onChange);  
+    },
+    componentWillUnmount: function() {
+        ForumStore.removeListener(this._onChange);
+    },
+    render: function () {
+        return (
+            <div>
+                <ForumHeader />
+            
+                <div className="container">
+                    <ForumQuestion />
+                    <hr />
+                    <ForumAnswers allAnswers={ this.state.allAnswers } />
+                    <hr />
+                    <h4>Add an answer</h4>
+                    <ForumAddAnswerBox onAddAnswer={ this._onAddAnswer } />
+                </div>
+            </div>
+        );
+    },
+    _onChange: function() {
+        this.setState({ allAnswers: ForumStore.getAnswers()});
+    },
+    _onAddAnswer: function(answerText) {
+        ForumDispatcher.dispatch({
+           actionType: 'FORUM_ANSWER_ADDED',
+            newAnswer: answerText
+        });
+    }
+});
